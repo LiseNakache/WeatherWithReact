05eee0026f6b9beefb58a4e35f1da8e671d48270
omer257 <omer257@gmail.com>
2017-07-23 18:33:28 +0300

    capatalized names

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import weatherApp from './weatherApp';
-import About from './weatherApp/common/About';
-import Page404 from './weatherApp/common/404';
+import WeatherApp from './WeatherApp';
+import About from './WeatherApp/common/About';
+import Page404 from './WeatherApp/common/404';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 const Routesss = () => (
     <div className="container">
         <Switch>
-            <Route name="home" exact path='/' component={weatherApp} />
+            <Route name="home" exact path='/' component={WeatherApp} />
             <Route name="about" exact path='/about' component={About} />
             <Route path="*" component={Page404}/>
         </Switch>
